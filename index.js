b882870c4d58ab086540e1a7a342b6c480a7efd9
fares <fareshasan7777@gmail.com>
2025-12-04 12:07:28 +0200

    add test endpoint

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 const app = require("./app");
 const PORT = process.env.PORT;
 
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
