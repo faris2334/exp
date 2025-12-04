b30a35a849e86aca2b1777c1251fa3c0b5c02ddf
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-12-05 00:19:52 +0200

    Add deleteTask route to taskRoutes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware'); 
-const { getTasksByProject, createTask, updateTaskStatus } = require('../controllers/tasksController');
+const { getTasksByProject, createTask, updateTaskStatus, deleteTask } = require('../controllers/tasksController');
 
 router.use(protect);
 
 router.get('/', getTasksByProject);  // GET /tasks?project_id=X
 router.post('/task/create', createTask);
 router.put('/task/update/:taskId', updateTaskStatus);
+router.delete('/task/delete/:taskId', deleteTask);
 
 module.exports = router;
