d7f63e4fe0f79bfc5782290daf48f871f1e3e0c6
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-12-06 18:07:11 +0200

    Add toggleLike route to commentRoutes

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { addComment, getComments , deleteComment } = require('../controllers/commentController');
+const { addComment, getComments, deleteComment, toggleLike } = require('../controllers/commentController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.use(protect);
 router.post('/comment/create/:taskId', addComment);
 router.get('/task/create/:taskId', getComments);
 router.delete('/delete/:commentId', deleteComment);
+router.post('/like/:commentId', toggleLike);
 
 module.exports = router;
